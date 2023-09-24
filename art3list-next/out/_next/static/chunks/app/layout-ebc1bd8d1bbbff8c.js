530cdd692b3978b2dcc73d69100d145f46cd71c0
juanseromo <juanseromo12@gmail.com>
2023-09-24 11:36:57 -0400

    added out

---
+++ art3list-next/out/_next/static/chunks/app/layout-ebc1bd8d1bbbff8c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1805:function(n,e,u){Promise.resolve().then(u.t.bind(u,3054,23))},3054:function(){}},function(n){n.O(0,[971,864,744],function(){return n(n.s=1805)}),_N_E=n.O()}]);
